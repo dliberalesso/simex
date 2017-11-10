3cfc2d9b70a0e13bef2ba8eeea60e589f3b65d57
Douglas Liberalesso <dliberalesso@gmail.com>
2017-11-10 01:21:27 -0200

    Extract generate_state into a named function

--- lib/simex.ex
+++ lib/simex.ex
@@ -8,19 +8,19 @@ defmodule SimEx do
   end
 
   def spawn_clients(number) do
-    generate_state = fn(_) ->
-      sleeptime = rand()
-      first = :crypto.strong_rand_bytes(32) |> Base.encode64 |> binary_part(0, 32)
-      client_id = "#{first}-#{sleeptime}"
-      %{client_id: client_id, sleeptime: sleeptime}
-    end
-
     Enum.to_list(1..number)
     |> Flow.from_enumerable
-    |> Flow.map(generate_state)
+    |> Flow.map(fn _ -> generate_state() end)
     |> Flow.partition
     |> Flow.map(fn state -> SimEx.Supervisor.start_client(state) end)
     |> Flow.run
+  end
+
+  def generate_state do
+    sleeptime = rand()
+    first = :crypto.strong_rand_bytes(32) |> Base.encode64 |> binary_part(0, 32)
+    client_id = "#{first}-#{sleeptime}"
+    %{client_id: client_id, sleeptime: sleeptime}
   end
 
   @doc """
