c5c4624037f25ec34247d0accdd17cf4b41de46d
Douglas Liberalesso <dliberalesso@gmail.com>
2017-11-19 16:20:44 -0200

    Fix typos and do a minor refactoring

--- lib/simex/random.ex
+++ lib/simex/random.ex
@@ -2,20 +2,20 @@ defmodule SimEx.Random do
   def rand do
     default = {:normal, [mean: 0, sd: 1]} # standard normal distribution
     Application.get_env(:simex, :distribution, default)
-    |> rand
+    |> do_rand
   end
 
-  defp rand({:normal, [mean: mean, sd: sd]}) do
+  defp do_rand({:normal, [mean: mean, sd: sd]}) do
     :rand.normal(mean, :math.pow(sd, 2))
     |> round
   end
 
-  defp rand({:triangle, [a: a, b: b, c: c]}) do
-    r = (1.0 - rand:uniform())
+  defp do_rand({:triangle, [a: a, b: b, c: c]}) do
+    r = (1.0 - :rand.uniform())
     if (r < ((c - a) / (b - a))) do
       a + :math.sqrt(r * (b - a) * (c - a))
-    else do
-      b - :math:sqrt((1 - r) * (b - a) * (b - c))
+    else
+      b - :math.sqrt((1 - r) * (b - a) * (b - c))
     end
   end
 
